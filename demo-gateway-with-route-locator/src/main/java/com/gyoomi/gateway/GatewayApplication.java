0b17932f925bd44b7ea80ec3ea831fa56e181643
leon <gyoomi0709@foxmail.com>
2019-10-14 23:10:01 +0800

    Initial commit

---
+++ demo-gateway-with-route-locator/src/main/java/com/gyoomi/gateway/GatewayApplication.java
@@ -0,0 +1,38 @@
+package com.gyoomi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2019/10/14 22:15
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    /**
+     * 基本转发
+     * 当访问http://localhost:8080/jd
+     * 转发到http://jd.com
+     *
+     * @param builder b
+     * @return RouteLocator
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/jd")
+                        .uri("http://jd.com:80")
+                        .id("jd_route")
+                ).build();
+    }
+}
---
+++ demo-gateway-with-yml-config/src/main/java/com/gyoomi/gateway/YmlGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.gyoomi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2019/10/14 22:24
+ */
+@SpringBootApplication
+public class YmlGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmlGatewayApplication.class, args);
+    }
+}
